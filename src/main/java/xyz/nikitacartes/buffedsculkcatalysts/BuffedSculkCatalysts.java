bfbfed0f57644cc43833e80e1580cde752a3e1d5
NikitaCartes <nikich98@yandex.ru>
2022-07-21 22:29:52 +0300

    Remove Main file because it's not needed

--- src/main/java/xyz/nikitacartes/buffedsculkcatalysts/BuffedSculkCatalysts.java
+++
@@ -1,9 +0,0 @@
-package xyz.nikitacartes.buffedsculkcatalysts;
-
-import net.fabricmc.api.ModInitializer;
-
-public class BuffedSculkCatalysts implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
